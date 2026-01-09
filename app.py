ef5f16ae1874268e28395a499ecf55586ab0025e
priyankags1402 <priyankags1402@gmail.com>
2026-01-09 12:53:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+# Test Cloud Build trigger
 
 app = Flask(__name__)
 
@@ -33,3 +34,4 @@ def demo():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
